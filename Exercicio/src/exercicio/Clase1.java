8e3101a63257bbfdc5e4f90ab82c0d1165cd9dd6
mfrancomartinez <mfrancomartinez@victoria27.danielcastelao.org>
2018-03-01 08:53:51 +0100

    Boletin_6_1_2

---
+++ Exercicio/src/exercicio/Clase1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio;
+
+/**
+ *
+ * @author mfrancomartinez
+ */
+public class Clase1 {
+    private double valor = 9.8;
+    private int x = 7;
+    
+    public void imprimir (double valor, int x){
+        System.out.print(valor+" "+this.x);
+    }
+}
