a481c869e9d4412dc7f15f76ab4e589c61b979d0
mfrancomartinez <mfrancomartinez@victoria27.danielcastelao.org>
2018-03-01 08:53:29 +0100

    Boletin_6_1

---
+++ Exercicio/src/exercicio/Exercicio.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio;
+
+/**
+ *
+ * @author mfrancomartinez
+ */
+public class Exercicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Clase1 obx1 = new Clase1();
+     
+     obx1.imprimir(24.3,5);
+     
+     /*El resultado es 24.3 7*/
+    }
+    
+}
