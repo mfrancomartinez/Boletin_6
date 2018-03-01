e3cf967670a389307df51f7597c115134de24699
mfrancomartinez <mfrancomartinez@victoria27.danielcastelao.org>
2018-03-01 13:59:23 +0100

    Boletin_6_4_2

---
+++ Boletin_6_4/src/boletin_6_4/Clase1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_6_4;
+
+/**
+ *
+ * @author mfrancomartinez
+ */
+public class Clase1 {    
+static int prop=0; 
+ public Clase1 (int valor){ 
+ prop=valor; 
+ } 
+ public int valorprop(){ 
+ return prop; 
+ } 
+} 
+
