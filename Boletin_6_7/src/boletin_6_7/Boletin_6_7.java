c365c3bf543df3d21f3ad2209904113995938d1b
mfrancomartinez <mfrancomartinez@victoria27.danielcastelao.org>
2018-03-05 09:07:20 +0100

    Boletin_6_7

---
+++ Boletin_6_7/src/boletin_6_7/Boletin_6_7.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_6_7;
+
+/**
+ *
+ * @author mfrancomartinez
+ */
+public class Boletin_6_7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Productos ex1 = new Productos();
+    ex1.imprimirfactura();
+    ex1.imprimirfactura2();
+    }
+    
+}
